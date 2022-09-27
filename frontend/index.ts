6d2239a04631ed44efb8576fba7334a911205486
sasha <sasha.luhr@gmail.com>
2022-09-27 17:04:11 +0200

    working implementation

---
+++ frontend/index.ts
@@ -0,0 +1,74 @@
+// import apiCall from "./api-utils";
+
+
+const apiCall = (
+    route: string, method: string, body: string
+) => {
+    fetch(`http://localhost:5050/api/${route}`, {
+        method,
+        body
+    }).then(function (response) {
+        if (response.ok) {
+            return response.json();
+        } else {
+            return Promise.reject(response);
+        }
+    }).then(function (data) {
+        // This is the JSON from our response
+        console.log(data);
+    }).catch(function (err) {
+        // There was an error
+        console.warn('Something went wrong.', err);
+    });
+};
+
+const boardButton = document.getElementById("submit-board");
+
+boardButton?.addEventListener("click", () => {
+    const width = <HTMLInputElement>document.getElementById("startWidth");
+    const height = <HTMLInputElement>document.getElementById("startHeight");
+
+    const data = `${width?.value} ${height?.value}`
+
+    console.log(data);
+    
+
+    const res = apiCall("board", "POST", data);
+    console.log(res);
+
+
+});
+
+const robotButton = document.getElementById("submit-robot");
+
+robotButton?.addEventListener("click", () => {
+    const width = <HTMLInputElement>document.getElementById("robotWidth");
+    const height = <HTMLInputElement>document.getElementById("robotHeight");
+    const direction = <HTMLInputElement>document.getElementById("robotDirection");
+
+    const data = `${width?.value} ${height?.value} ${direction?.value}`
+
+    console.log(data);
+    
+
+    const res = apiCall("robot", "POST", data);
+    console.log(res);
+
+
+});
+
+const moveButton = document.getElementById("submit-movement");
+
+moveButton?.addEventListener("click", () => {
+    const movement = <HTMLInputElement>document.getElementById("robotWidth");
+
+    const data = movement?.value;
+
+    console.log(data);
+    
+
+    const res = apiCall("move", "POST", data);
+    console.log(res);
+
+
+});
--- server/index.ts
+++ server/index.ts
@@ -8,17 +8,74 @@ const startServer = (
 ) => {
     console.log(boardState);
     console.log(port);
-    
+
     const server = http.createServer((req, res) => {
+        const headers = {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "POST, GET",
+            "Access-Control-Max-Age": 2592000,
+        };
 
-        if (req.url === '/api/positions' && req.method === 'POST') {
-            const [width, height] = parseBoardInput(line);
-            boardState.width = width;
-            boardState.height = height;
-            console.log(boardState);
-        } else if (req.url === '/api/products' && req.method === 'POST') {
+        if (req.url === '/api/board' && req.method === 'POST') {
 
-        }
+            let body = ''
+            req.on('data', (data) => {
+                body += data
+                console.log('Partial body: ' + body)
+            })
+            req.on('end', () => {
+
+                console.log(body);
+
+                const [width, height] = parseBoardInput(body);
+                boardState.width = width;
+                boardState.height = height;
+
+                console.log(boardState);
+                res.writeHead(200, { ...headers, 'Content-Type': 'application/json' });
+                res.end(JSON.stringify({ body: boardState }));
+            });
+
+
+        } else if (req.url === '/api/robot' && req.method === 'POST') {
+            let body = ''
+            req.on('data', (data) => {
+                body += data
+                console.log('Partial body: ' + body)
+            })
+            req.on('end', () => {
+
+                console.log(body);
+
+                const [robotW, robotH, robotDirection] = parseStartingPositionInput(body);
+                boardState.robotW = robotW;
+                boardState.robotH = robotH;
+                boardState.robotDirection = robotDirection;
+                console.log(boardState);
+
+                res.writeHead(200, { ...headers, 'Content-Type': 'application/json' });
+                res.end(JSON.stringify({ body: boardState }));
+            });
+
+        } else if (req.url === '/api/move' && req.method === 'POST') {
+            let body = ''
+            req.on('data', (data) => {
+                body += data
+                console.log('Partial body: ' + body)
+            })
+            req.on('end', () => {
+
+                console.log(body);
+
+                parseMovement(body, boardState);
+                console.log(boardState);
+
+
+                res.writeHead(200, { ...headers, 'Content-Type': 'application/json' });
+                res.end(JSON.stringify({ body: boardState }));
+            });
+
+        } 
 
     });
 
